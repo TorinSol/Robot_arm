f9d75f728d38c57d52ffa6426df31fcf76bae8de
Sol <ethan.ricker567@gmail.com>
2025-09-26 23:42:46 -0400

    removing test function

--- main.py
+++ main.py
@@ -38,26 +38,6 @@ Takes an image and draws the skeleton on the detected persons
 :returns the marked up image
 """
 
-def draw_landmarks_on_image_1(rgb_image, detection_result_func):
-  pose_landmarks_list = detection_result_func.pose_landmarks
-  annotated_image_func = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image_func,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image_func
 
 
 def draw_landmarks_on_image(rgb_image, detection_result_func):
